6f196155f09211be27f0db3e66340ed03cc8a75e
leader-akash <leader.akashak@gmail.com>
2023-06-30 20:07:27 +0530

    create homePage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
 import './App.css';
+import { ReactDOM } from 'react';
+import Navbar from './header/Header';
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <h1>Classy </h1>
+    // <h1>Classy </h1>,
+    // <Navbar />
+    <HomePage />
   );
 }
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import image from '../images/img4.png';  
+import "./HomePage.css";
+
+const MainPage = () =>{
+    return (
+    <>
+	    <div class="nav-two-container">
+            <div class="logo">Classy Store</div>
+                <div class="search-bar">
+                    <input class="search-input" type="text" placeholder="Search classy-store.com" />
+                    <i class="fa-solid fa-magnifying-glass "></i>
+                </div>
+    <div class="item-list">
+    <span class="badge-icon">
+    <span class="badge red">20+</span>
+    <i class="fa-regular fa-heart"> </i>
+    </span>
+    <span class="badge-icon">
+    <span class="badge blue">20+</span>
+    <i class="fa-solid fa-cart-shopping " ></i>
+    </span>
+    
+    <button class="sign-in">
+    <i class="fa-regular fa-user"></i>
+    Sign In</button>
+    </div>
+    </div>
+
+<div className="welcome">
+    <img className="img-settings" src={image} alt="img here"/>
+ 
+    Welcome to the <span className="logo-bold">Classy Store !</span>
+
+    <div className="greeting">Exciting Products, Exciting Deals, Exciting Discounts !!</div>
+
+    <button className="explore">Explore</button>
+
+    <div className="comment">Made with 💖 by Akash</div>
+</div>
+
+
+    </>
+);
+
+}
+
+export default MainPage;
